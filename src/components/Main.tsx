c03e7cfd6398a37be5d252439a6462fbc43d13c3
Spyei <caio.salamao1234@gmail.com>
2023-06-20 16:49:59 -0300

    improving hovers to icons

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,22 +2,107 @@ import githubIcon from "../assets/svg/github.svg";
 import linkedinIcon from "../assets/svg/linkedin.svg";
 import instagramIcon from "../assets/svg/instagram.svg";
 import whatsappIcon from "../assets/svg/whatsapp.svg";
+import React, { useState } from "react";
 
-export const Main = () => {
+export const Main: React.FC = () => {
+    const [isHovering, setHover] = useState<boolean>(true);
+
     return (
         <section className="h-[100vh] bg-[#000] from-[1%] w-full flex-row my:flex-col-reverse flex items-center justify-center">
-            <div className="flex flex-col my:flex-row my:items-center my:justify-center gap-3 mr-4 my:h-[100px] h-[180px]">
-                <a href="https://github.com/spyei">
-                    <img className="h-[30px]" src={githubIcon} alt="Github Icon" />
+            <div
+                className={`flex text-white flex-col my:flex-row my:items-center my:justify-center mr-4 my:h-[100px] h-[180px]`}
+            >
+                <a
+                    onMouseEnter={() => {
+                        setHover(true);
+                    }}
+                    onMouseLeave={() => {
+                        setHover(false);
+                    }}
+                    className="text-white w-[100%] flex gap-2 items-center justify-start transition-all h-[40px] duration-300"
+                    href="https://github.com/spyei"
+                >
+                    <img
+                        className="h-[30px]"
+                        src={githubIcon}
+                        alt="Github Icon"
+                    />
+                    <div
+                        className={`transition-all duration-300 text-center flex items-center justify-center  text-neutral-300 hover:text-white ${
+                            isHovering ? "text-[16px] ml-2" : "text-[0px]"
+                        }`}
+                    >
+                        Github
+                    </div>
                 </a>
-                <a href="/">
-                    <img className="h-[30px]" src={linkedinIcon} alt="Linkedin Icon" />
+                <a
+                    onMouseEnter={() => {
+                        setHover(true);
+                    }}
+                    onMouseLeave={() => {
+                        setHover(false);
+                    }}
+                    className="w-[100%] flex gap-2 items-center justify-start transition-all h-[40px] duration-300"
+                    href=""
+                >
+                    <img
+                        className="h-[30px]"
+                        src={linkedinIcon}
+                        alt="Linkedin Icon"
+                    />
+                    <div
+                        className={`transition-all duration-300 text-center  text-neutral-300 hover:text-white ${
+                            isHovering ? "text-[16px] ml-1" : "text-[0px]"
+                        }`}
+                    >
+                        Linkedin
+                    </div>
                 </a>
-                <a href="https://www.instagram.com/caiuwu_/">
-                    <img className="h-[30px]" src={instagramIcon} alt="Instagram Icon" />
+                <a
+                    onMouseEnter={() => {
+                        setHover(true);
+                    }}
+                    onMouseLeave={() => {
+                        setHover(false);
+                    }}
+                    className="w-[100%] flex gap-2 items-center justify-start transition-all h-[40px] duration-300"
+                    href="https://www.instagram.com/caiuwu_/"
+                >
+                    <img
+                        className="h-[30px]"
+                        src={instagramIcon}
+                        alt="Instagram Icon"
+                    />
+                    <div
+                        className={`transition-all duration-300 text-center  text-neutral-300 hover:text-white ${
+                            isHovering ? "text-[16px]" : "text-[0px]"
+                        }`}
+                    >
+                        Instagram
+                    </div>
                 </a>
-                <a href="/">
-                    <img className="h-[30px]" src={whatsappIcon} alt="Whatsapp Icon" />
+                <a
+                    onMouseEnter={() => {
+                        setHover(true);
+                    }}
+                    onMouseLeave={() => {
+                        setHover(false);
+                    }}
+                    className="w-[100%] flex gap-2 items-center justify-start transition-all h-[40px] duration-300"
+                    href="/"
+                >
+                    <img
+                        className="h-[30px]"
+                        src={whatsappIcon}
+                        alt="Whatsapp Icon"
+                    />
+                    <div
+                        className={`transition-all duration-300 text-center  text-neutral-300 hover:text-white ${
+                            isHovering ? "text-[16px]" : "text-[0px]"
+                        }`}
+                    >
+                        Whatsapp
+                    </div>
                 </a>
             </div>
             <div>
