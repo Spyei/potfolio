89aa080744bc77657ebbea2ad9d3dc47825cdcde
Spyei <caio.salamao1234@gmail.com>
2023-06-22 13:58:30 -0300

    adding opacity transition to button

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,14 @@ export const Projects: React.FC<{
     setPage: (value: number) => void;
 }> = ({ setPage }) => {
     return (
-        <div>oi</div>
-    )
-}
+        <section className="bg-black w-[100vw] text-white h-[100vh]">
+            <button
+                onClick={() => {
+                    setPage(1);
+                }}
+            >
+                Voltar
+            </button>
+        </section>
+    );
+};
