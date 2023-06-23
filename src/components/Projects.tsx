048214aa94c82455eede26b94aebc34b04467db4
Spyei <caio.salamao1234@gmail.com>
2023-06-23 15:42:49 -0300

    improving project card

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,22 +27,26 @@ export const Projects: React.FC<{
         <section className="bg-black w-[100vw] text-white h-[100vh]">
             <div className="flex items-center justify-center flex-col">
                 <div className="flex flex-col items-center">
-                    <h1 className="text-[50px]">Projetos em destaque</h1>
-                    <hr className="w-[90vw] m-2" />
+                    <h1 className="text-[43px] my-3">
+                        Projetos em desenvolvimento
+                    </h1>
+                    <hr className="w-[90vw] mb-6" />
                 </div>
-                <button className="flex flex-row bg-neutral-900 transition-colors duration-300 hover:bg-neutral-800 rounded-lg border-2 border-white">
+                <button className="flex flex-col bg-neutral-900 transition-colors duration-300 hover:bg-neutral-800 rounded-lg border-2 border-white">
                     <div className="p-2 flex flex-col gap-1">
-                        <h1 className="text-[19px] text-left">Botlist-Website</h1>
+                        <h1 className="text-[19px] text-left">
+                            Botlist-Website
+                        </h1>
                         <div>
                             Um site feito para divulgar seu bot de discord de
                             uma maneira simples.
                         </div>
                     </div>
-                    <img
-                        className="w-[200px] h-[100px] rounded-r-lg p-0"
-                        src="https://cdn.discordapp.com/attachments/955900016436838440/1121497825272926268/image.png"
-                        alt="Botlist-Website preview"
-                    />
+                    <div className="flex mx-3 mb-3 gap-[6px] bg-neutral-800 p-[6px] rounded-lg border-2">
+                        <i className="devicon-tailwindcss-plain colored"></i>
+                        <i className="devicon-typescript-plain colored"></i>
+                        <i className="devicon-react-plain colored"></i>
+                    </div>
                 </button>
             </div>
             {repos ? <div>repos aqui</div> : <div>Loading...</div>}
