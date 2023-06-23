548f2c09c9ef234c7a62d77addc507a32ee33cf8
Spyei <caio.salamao1234@gmail.com>
2023-06-23 16:50:13 -0300

    puting main in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Main } from "./components/Main";
-import "./index.css";
+import { useState } from "react";
+import { InitInfos } from "./components/InitInfos";
+import { Projects } from "./components/Projects";
 
-function App() {
-    return (
-        <body>
-            <Main />
-        </body>
-    );
-}
+function App () {
+    const [page, setPage] = useState<number>(1);
+
+    switch (page) {
+        case 1:
+            return <InitInfos setPage={setPage} />;
+        case 2:
+            return <Projects setPage={setPage} />;
+    }
+};
 
 export default App;
