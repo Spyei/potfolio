fa86c339401250e584d3167ad06fe5e0eb8cd963
Spyei <caio.salamao1234@gmail.com>
2023-10-07 16:19:21 -0300

    feat: New icons to project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,12 +1,21 @@
 import { FC } from "react";
 import Project from "./Project";
+import * as icon from "react-icons/bi";
 
 const Projects: FC = () => {
     return (
         <section className="bg-white h-[100vh] w-[100vw]">
-            <div className="w-full h-full flex-row grid grid-cols-2 my:grid-cols-1 place-items-center items-center">
-                <Project name="Botlist Website" project="https://simo-botlist.vercel.app" repo="https://github.com/Simo-Workspace/Botlist-Website" img="https://cdn.discordapp.com/attachments/1125236198810599534/1131609026480844921/image.png"/>
-                <Project name="TScord docs" project="https://tscord.vercel.app" repo="https://github.com/ts-cord/tscord-docs" img="https://cdn.discordapp.com/attachments/1125236198810599534/1131609026480844921/image.png"/>
+            <div className="w-[100vw] h-full flex items-center flex-col justify-center gap-[100px]">
+                <div className="w-full flex justify-center text-6xl font-extrabold">
+                    <h1>Meus projetos</h1>
+                </div>
+                <Project name="Simo Botlist" description="Um site feito para ajudar a divulgação do seu bot de discord." project="https://simo-botlist.vercel.app" repo="https://github.com/Simo-Workspace/Botlist-Website" img="https://cdn.discordapp.com/attachments/955900016436838440/1160277222620405870/image.png?ex=6534135e&is=65219e5e&hm=6ee8a179bac146ad3891fd5f2c2b66882711f4e1964dffd99a75b3b3f2b5a3b8&"
+                    stacks={[
+                        <icon.BiLogoTypescript fill="#0379CC" size={35} />,
+                        <icon.BiLogoReact fill="#00D8FE" size={35} />,
+                        <icon.BiLogoTailwindCss fill="#38BDF8" size={35} />
+                    ]}
+                />
             </div>
         </section>
     )
