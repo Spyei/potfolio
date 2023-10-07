652026f2a42f6073091d45c45c5e568838f34a57
Spyei <caio.salamao1234@gmail.com>
2023-10-07 17:45:42 -0300

    feat: Underline to title

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -6,8 +6,9 @@ const Projects: FC = () => {
     return (
         <section className="bg-white h-[100vh] w-[100vw]">
             <div className="w-[100vw] h-full flex items-center flex-col justify-center gap-[100px]">
-                <div className="w-full flex justify-center text-6xl font-extrabold">
-                    <h1>Meus projetos</h1>
+                <div className="flex justify-center text-6xl font-extrabold flex-col items-center gap-4">
+                    <h1>Projetos</h1>
+                    <div className="bg-black w-full h-[5px] rounded-lg" />
                 </div>
                 <Project name="Simo Botlist" description="Um site feito para ajudar a divulgação do seu bot de discord." project="https://simo-botlist.vercel.app" repo="https://github.com/Simo-Workspace/Botlist-Website" img="https://cdn.discordapp.com/attachments/955900016436838440/1160277222620405870/image.png?ex=6534135e&is=65219e5e&hm=6ee8a179bac146ad3891fd5f2c2b66882711f4e1964dffd99a75b3b3f2b5a3b8&"
                     stacks={[
